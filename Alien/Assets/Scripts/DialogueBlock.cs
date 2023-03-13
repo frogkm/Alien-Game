66078477875561b48451a061a8637898dfbfba49
monten42 <kiefermontenero@gmail.com>
2023-03-13 18:39:08 -0400

    changes

---
+++ Alien/Assets/Scripts/DialogueBlock.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueBlock : MonoBehaviour
+{
+    [SerializeField] private string dialogue;
+}
---
+++ Alien/Assets/Scripts/DialogueObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueObject : MonoBehaviour
+{
+    [SerializeField] private List<DialogueBlock> dialogueLine;
+
+}
